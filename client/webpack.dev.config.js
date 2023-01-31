d93877f472e065e2eaa873c7d09171acf20e1373
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-01-31 07:24:34 +0200

    feat: add initial setup

---
+++ client/webpack.dev.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    port: 3000,
+    historyApiFallback: true,
+  },
+};
