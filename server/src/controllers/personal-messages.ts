9ee9b0c75b723aaee3cd882b397ef12acc880133
Alex89198900 <alexchorn1989@ukr.net>
2023-02-03 21:35:35 +0200

    fix: delete comments

--- server/src/controllers/personal-messages.ts
+++ server/src/controllers/personal-messages.ts
@@ -59,53 +59,6 @@ const createPersonalMessage: Handler = (req, res, next) => {
     });
 };
 
-// exports.createPost = (req, res, next) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     const error = new Error('Validation failed, entered data is incorrect.');
-//     error.statusCode = 422;
-//     throw error;
-//   }
-//   if (!req.file) {
-//     const error = new Error('No image provided.');
-//     error.statusCode = 422;
-//     throw error;
-//   }
-//   const imageUrl = req.file.path;
-//   const title = req.body.title;
-//   const content = req.body.content;
-//   let creator;
-//   const post = new Post({
-//     title: title,
-//     content: content,
-//     imageUrl: imageUrl,
-//     creator: req.userId
-//   });
-//   post
-//     .save()
-//     .then(result => {
-//       return User.findById(req.userId);
-//     })
-//     .then(user => {
-//       creator = user;
-//       user.posts.push(post);
-//       return user.save();
-//     })
-//     .then(result => {
-//       res.status(201).json({
-//         message: 'Post created successfully!',
-//         post: post,
-//         creator: { _id: creator._id, name: creator.name }
-//       });
-//     })
-//     .catch(err => {
-//       if (!err.statusCode) {
-//         err.statusCode = 500;
-//       }
-//       next(err);
-//     });
-// };
-
 const getPersonalMessage: Handler = (req, res, next) => {
   const personalMessageId = req.params.id;
   PersonalMessage.findById(personalMessageId)
@@ -115,7 +68,7 @@ const getPersonalMessage: Handler = (req, res, next) => {
         // error.statusCode = 404;
         throw error;
       }
-      res.status(200).json({ message: 'Message fetched.', messageId: message });
+      res.status(200).json({ messageInfo: 'Message fetched.', message: message });
     })
     .catch((err) => {
       if (!err.statusCode) {
@@ -137,22 +90,16 @@ const updatePersonalMessage: Handler = (req, res, next) => {
   PersonalMessage.findById(personalMessageId)
     .then((message) => {
       if (!message) {
-        const error = new Error('Could not find post.');
+        const error = new Error('Could not find message.');
         // error.statusCode = 404;
         throw error;
       }
-      // if (!message.responsedMessageId || message.responsedMessageId.toString() !== personalMessageId) {
-      //   const error = new Error('Not authorized!');
-      //   // error.statusCode = 403;
-      //   throw error;
-      // }
-
       const contentMessage = req.body.message;
       message.message = contentMessage;
       return message.save();
     })
     .then((result) => {
-      res.status(200).json({ message: 'Message updated!', messageRes: result });
+      res.status(200).json({ messageInfo: 'Message updated!', message: result });
     })
     .catch((err) => {
       if (!err.statusCode) {
@@ -164,37 +111,24 @@ const updatePersonalMessage: Handler = (req, res, next) => {
 
 const deletePersonalMessage: Handler = (req, res, next) => {
   const personalMessageId = req.params.id;
-  PersonalMessage.findById(personalMessageId).then((message) => {
-    if (!message) {
-      const error = new Error('Could not find message.');
-      // error.statusCode = 404;
-      throw error;
-    }
-    // if (post.creator.toString() !== req.userId) {
-    //   const error = new Error('Not authorized!');
-    //   error.statusCode = 403;
-    //   throw error;
-    // }
-    // Check logged in user
-
-    return PersonalMessage.findByIdAndRemove(personalMessageId);
-  });
-  // .then((result) => {
-  //   return User.findById(req.);
-  // })
-  // .then((user) => {
-  //   user.posts.pull(postId);
-  //   return user.save();
-  // })
-  // .then((result) => {
-  //   res.status(200).json({ message: 'Deleted post.' });
-  // })
-  // .catch((err) => {
-  //   if (!err.statusCode) {
-  //     err.statusCode = 500;
-  //   }
-  //   next(err);
-  // });
+  PersonalMessage.findById(personalMessageId)
+    .then((message) => {
+      if (!message) {
+        const error = new Error('Could not find message.');
+        // error.statusCode = 404;
+        throw error;
+      }
+      return PersonalMessage.findByIdAndRemove(personalMessageId);
+    })
+    .then((result) => {
+      res.status(200).json({ messageInfo: 'Deleted message.' });
+    })
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
 export default {
