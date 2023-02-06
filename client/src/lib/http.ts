18c34c4e67a2acccd4f85a996af85233e1cbcd1a
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-02-06 13:28:35 +0200

    feat: app http client

---
+++ client/src/lib/http.ts
@@ -0,0 +1,109 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios';
+
+enum ErrorStatusCode {
+  Unauthorized = 401,
+  Forbidden = 403,
+  TooManyRequests = 429,
+  InternalServerError = 500,
+}
+
+const headers: Readonly<Record<string, string | boolean>> = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json; charset=utf-8',
+  'Access-Control-Allow-Credentials': true,
+  'X-Requested-With': 'XMLHttpRequest',
+};
+
+// We can use the following function to inject the JWT token through an interceptor
+// We get the `accessToken` from the localStorage that we set when we authenticate
+const injectToken = (config: AxiosRequestConfig = {}): AxiosRequestConfig => {
+  try {
+    const token = localStorage.getItem('accessToken');
+    config.headers = {};
+    if (token != null) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  } catch (error) {
+    console.error(error);
+  }
+  return config;
+};
+
+class Http {
+  private instance: AxiosInstance | null = null;
+
+  private get http(): AxiosInstance {
+    return this.instance != null ? this.instance : this.initHttp();
+  }
+
+  initHttp() {
+    const http = axios.create({
+      baseURL: 'https://api.example.com',
+      headers,
+      withCredentials: true,
+    });
+
+    // http.interceptors.request.use(injectToken, (error: AxiosError) => Promise.reject(error));
+
+    http.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        const { response } = error;
+        return this.handleError(response);
+      }
+    );
+
+    this.instance = http;
+    return http;
+  }
+
+  request<T = unknown, R = AxiosResponse<T>>(config: AxiosRequestConfig): Promise<R> {
+    return this.http.request(config);
+  }
+
+  get<T = unknown, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
+    return this.http.get<T, R>(url, config);
+  }
+
+  post<T = unknown, R = AxiosResponse<T>>(url: string, data?: T, config?: AxiosRequestConfig): Promise<R> {
+    return this.http.post<T, R>(url, data, config);
+  }
+
+  put<T = unknown, R = AxiosResponse<T>>(url: string, data?: T, config?: AxiosRequestConfig): Promise<R> {
+    return this.http.put<T, R>(url, data, config);
+  }
+
+  delete<T = unknown, R = AxiosResponse<T>>(url: string, config?: AxiosRequestConfig): Promise<R> {
+    return this.http.delete<T, R>(url, config);
+  }
+
+  // Handle global app errors
+  // We can handle generic app errors depending on the status code
+  private handleError(error: AxiosError) {
+    const { status } = error;
+
+    switch (status) {
+      case ErrorStatusCode.InternalServerError: {
+        // Handle InternalServerError
+        break;
+      }
+      case ErrorStatusCode.Forbidden: {
+        // Handle Forbidden
+        break;
+      }
+      case ErrorStatusCode.Unauthorized: {
+        // Handle Unauthorized
+        break;
+      }
+      case ErrorStatusCode.TooManyRequests: {
+        // Handle TooManyRequests
+        break;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+}
+
+export const http = new Http();
