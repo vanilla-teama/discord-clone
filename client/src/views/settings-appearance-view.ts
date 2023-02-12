88ac6f87d355a3e4105707d9fee91364331299c2
Alex89198900 <alexchorn1989@ukr.net>
2023-02-12 16:59:56 +0200

    feat: add choise theme

--- client/src/views/settings-appearance-view.ts
+++ client/src/views/settings-appearance-view.ts
@@ -4,7 +4,9 @@ import MainView from './main-view';
 import SettingsScreenView from './settings-screen-view';
 
 class SettingsAppearanceView extends View {
-  static readonly classNames = {};
+  static readonly classNames = {
+    container: 'apperance-contauner',
+  };
 
   constructor() {
     const $root = SettingsScreenView.$content;
@@ -14,6 +16,21 @@ class SettingsAppearanceView extends View {
     super($root);
   }
   build(): void {
+    const $appcontainer = $('div', SettingsAppearanceView.classNames.container);
+    const $containerTheme = $('div', SettingsAppearanceView.classNames.container);
+    const $containerThemeLight = $('div', SettingsAppearanceView.classNames.container);
+    const $inputThemeLight = Object.assign($('input'), { id: 'light', type: 'radio', name: 'radio-theme' });
+    const $labelThemeLight = Object.assign($('label'), { htmlFor: 'light', textContent: 'Светлая' });
+    $containerThemeLight.append($inputThemeLight, $labelThemeLight);
+    const $containerThemeDark = $('div', SettingsAppearanceView.classNames.container);
+    const $inputThemeDark = Object.assign($('input'), { id: 'light', type: 'radio', name: 'radio-theme' });
+    const $labelThemeDark = Object.assign($('label'), { htmlFor: 'light', textContent: 'Темная' });
+    $containerThemeDark.append($inputThemeDark, $labelThemeDark);
+    const $containerThemeSinhronization = $('div', SettingsAppearanceView.classNames.container);
+    const $inputThemeSinhronization = Object.assign($('input'), { id: 'light', type: 'radio', name: 'radio-theme' });
+    const $labelThemeSinhronization = Object.assign($('label'), { htmlFor: 'light', textContent: 'Темная' });
+    $containerThemeSinhronization.append($inputThemeSinhronization, $labelThemeSinhronization);
+    $containerTheme.append($containerThemeLight, $containerThemeDark, $containerThemeSinhronization);
     this.$container.append('I AM SETTINGS APPEARANCE!');
   }
 }
