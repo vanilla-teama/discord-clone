9fe5caff8dff12358b3ddcdbe3a1c4de2093f98d
AKiroi <alexandr.kiroi@gmail.com>
2023-02-15 16:17:58 +0200

    feat: add layout to server-main-content

--- client/src/components/servers-main-content.ts
+++ client/src/components/servers-main-content.ts
@@ -1,5 +1,5 @@
 import Controller from '../lib/controller';
-import ServersMainContentView from '../views/servers-main-content';
+import ServersMainContentView from '../views/servers-main-content-view';
 
 class ServersMainContentComponent extends Controller<ServersMainContentView> {
   constructor() {
