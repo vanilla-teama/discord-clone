55100b3c0bd92fe6550aafcdbcdfd04b2a131db0
Hlib Hodovaniuk <gleb.godovanyuk@gmail.com>
2023-02-17 22:54:58 +0200

    feat: add friends layout

--- client/src/views/friends-app-bar-view.ts
+++ client/src/views/friends-app-bar-view.ts
@@ -11,8 +11,15 @@ class FriendsAppBarView extends View {
     super($root);
   }
   async build(): Promise<void> {
-    const $navFriends = $('button');
-    const $navAddFriend = $('button');
+    const $friendsContainer = $('div', 'friends-app-bar__container');
+    const $friendsIconsBlock = $('div', 'friends-app-bar__icons-block');
+    const $friendsIcon = $('div', 'friends-app-bar__icon');
+    const $friendsTitle = $('div', 'friends-app-bar__title');
+    $friendsTitle.textContent = 'Friends';
+
+    const $friendsBtnBlock = $('div', 'friends-app-bar__buttons-block');
+    const $navFriends = $('button', 'friends-app-bar__friends-btn');
+    const $navAddFriend = $('button', 'friends-app-bar__add-btn');
 
     $navFriends.onclick = this.showFriends;
     $navAddFriend.onclick = this.showAddFriend;
@@ -20,7 +27,11 @@ class FriendsAppBarView extends View {
     $navFriends.textContent = 'Friends';
     $navAddFriend.textContent = 'Add Friend';
 
-    this.$container.append('Friends', $navFriends, $navAddFriend);
+    $friendsIconsBlock.append($friendsIcon, $friendsTitle);
+    $friendsBtnBlock.append($navFriends, $navAddFriend);
+    $friendsContainer.append($friendsIconsBlock, $friendsBtnBlock);
+
+    this.$container.append($friendsContainer);
   }
 
   showFriends = async (): Promise<void> => {};
