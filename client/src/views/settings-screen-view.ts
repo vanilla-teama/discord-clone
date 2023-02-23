d2f58f7f0756a10795156040f0f7f61431af3b0e
AKiroi <alexandr.kiroi@gmail.com>
2023-02-23 02:00:20 +0200

    feat: add styling to setting page

--- client/src/views/settings-screen-view.ts
+++ client/src/views/settings-screen-view.ts
@@ -4,8 +4,8 @@ import * as close from '../assets/icons/close-setting.svg';
 class SettingsScreenView extends View {
   static readonly classNames = {
     btnClose: 'btn-close-settings',
-    imgBtn: 'img-close-settings',
-    containerBtn: 'container-close-btn',
+    //imgBtn: 'img-close-settings',
+    //containerBtn: 'container-close-btn',
     portal: 'portal',
   };
 
@@ -24,16 +24,17 @@ class SettingsScreenView extends View {
   }
   build(): void {
     const $btnClose = this.$closeButton;
-    const $containerBtn = $('div', SettingsScreenView.classNames.containerBtn);
-    const $imgBtn = Object.assign($('img', SettingsScreenView.classNames.imgBtn), { src: close.default });
-    $btnClose.append($imgBtn);
-    $containerBtn.append($btnClose);
+    //const $containerBtn = $('div', SettingsScreenView.classNames.containerBtn);
+    //const $imgBtn = Object.assign($('img', SettingsScreenView.classNames.imgBtn), { src: close.default });
+    //$btnClose.append($imgBtn);
+    //$containerBtn.append($btnClose);
     const $container = $('div', 'settings');
     SettingsScreenView.$sidebar = $('div', 'settings__sidebar');
     SettingsScreenView.$content = $('div', 'settings__content');
     SettingsScreenView.$portal = $('div', SettingsScreenView.classNames.portal);
 
-    $container.append(SettingsScreenView.$sidebar, SettingsScreenView.$content, $containerBtn);
+    $container.append(SettingsScreenView.$sidebar, SettingsScreenView.$content, $btnClose);
+    //$container.append(SettingsScreenView.$sidebar, SettingsScreenView.$content, $containerBtn);
     this.$container.append($container, SettingsScreenView.$portal);
   }
 
